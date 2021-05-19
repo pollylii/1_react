b9c0ebebdf07e9a0fe3d57022a6096e4a7e920f1
CNT-HK\Polly Li <794211268@qq.com>
2021-05-19 15:57:33 +0800

    Hello Reat

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react'
+import Hello from './component/Hello'
+import Welcome from './component/Welcome'
 
-// 函数定义组件
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 类组件
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <Hello/>
+        <Welcome/>
+      </div>
+    )
+  }
 }
-//  暴露组件
+// 函数组件
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+//  暴露App组件
 export default App;
---
+++ src/component/Hello/index.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react'
+import './index.css'
+
+export default class Hello extends Component {
+    render() {
+        return(
+            <h1 className="hello">Hello React</h1>
+        )
+    }
+}
